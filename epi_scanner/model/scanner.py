d86fffbeb224986a735758be4164c747ecef43fe
Eduardo Correa Araujo <65051489+eduardocorrearaujo@users.noreply.github.com>
2023-11-23 09:24:12 -0300

    solve minor issue

--- epi_scanner/model/scanner.py
+++ epi_scanner/model/scanner.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 from pathlib import Path
-from epiweeks import Week
+
 import lmfit as lm
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from epiweeks import Week
 from lmfit import Parameters
 
 
@@ -53,8 +54,8 @@ def otim(df, t_ini, t_fin, verbose=False):
     df.reset_index(inplace=True)
     df["casos_cum"] = df.casos.cumsum()
     params = Parameters()
-    params.add('gamma', min = 0.3, max = 0.33)
-    #params.add("gamma", min=0.95, max=1.05)
+    params.add("gamma", min=0.3, max=0.33)
+    # params.add("gamma", min=0.95, max=1.05)
     params.add("L1", min=1.0, max=5e5)
     params.add("tp1", min=5, max=35)
     params.add("b1", min=1e-6, max=1)
@@ -87,33 +88,37 @@ def otim(df, t_ini, t_fin, verbose=False):
 
     return out, df
 
+
 def comp_duration(curve):
-    '''
+    """
     This function computes an estimation of the epidemic beginning,
     duration and end based of the peak of richards model estimated;
-    '''
-        
+    """
+
     df_aux = pd.DataFrame()
 
-    df_aux['dates'] = curve.iloc[:52].data_iniSE
-    #print(curve.columns)
-    df_aux['SE'] = [Week.fromdate(i).cdcformat() for i in df_aux['dates']]
-    #df_aux['richards'] = curve.richards
-    df_aux['diff_richards'] = np.concatenate(([0], np.diff(curve.richards)), axis=0)
+    df_aux["dates"] = curve.iloc[:52].data_iniSE
+    # print(curve.columns)
+    df_aux["SE"] = [Week.fromdate(i).cdcformat() for i in df_aux["dates"]]
+    # df_aux['richards'] = curve.richards
+    df_aux["diff_richards"] = np.concatenate(
+        ([0], np.diff(curve.richards)), axis=0
+    )
 
-    max_c = df_aux['diff_richards'].max()
+    max_c = df_aux["diff_richards"].max()
 
-    df_aux = df_aux.loc[ df_aux.diff_richards >= (0.05)*max_c].sort_index()
+    df_aux = df_aux.loc[df_aux.diff_richards >= (0.05) * max_c].sort_index()
 
-    ini = str(df_aux['SE'].values[0])
+    ini = str(df_aux["SE"].values[0])
 
-    end = str(df_aux['SE'].values[-1])
+    end = str(df_aux["SE"].values[-1])
 
     dur = int(end[-2:]) - int(ini[-2:])
-    
-    ep_dur = {'ini': ini, 'end': end, 'dur': dur}
-        
+
+    ep_dur = {"ini": ini, "end": end, "dur": dur}
+
     return ep_dur
+
 
 class EpiScanner:
     def __init__(self, last_week: int, data: pd.DataFrame, muninames):
@@ -128,7 +133,7 @@ class EpiScanner:
         data : pandas.DataFrame
             A pandas DataFrame containing the time series data for all cities.
         muninames: dict
-            A dictionary with the muninames and codes 
+            A dictionary with the muninames and codes
         """
         self.window = last_week
         self.data = data
@@ -184,16 +189,18 @@ class EpiScanner:
                 "sir_pars": get_SIR_pars(results.params.valuesdict()),
             }
         )
-        self.curves[geocode].append({
-                'year': year,
-                'df': curve,
-            
-                #  add residuals and comp duration 
-                'residuals': abs(curve.richards - curve.casos_cum),
-                'sum_res': sum(abs(curve.richards - curve.casos_cum))/max(curve.casos_cum),  
-                'ep_time': comp_duration(curve)
-                }) 
-        
+        self.curves[geocode].append(
+            {
+                "year": year,
+                "df": curve,
+                #  add residuals and comp duration
+                "residuals": abs(curve.richards - curve.casos_cum),
+                "sum_res": sum(abs(curve.richards - curve.casos_cum))
+                / max(curve.casos_cum),
+                "ep_time": comp_duration(curve),
+            }
+        )
+
     def get_residuals(self, geocode, year):
         """
         Get residuals for the years fitted curve from
@@ -201,13 +208,12 @@ class EpiScanner:
         """
         df = None
         for curve in self.curves[geocode]:
-            if year != curve['year']:
+            if year != curve["year"]:
                 continue
-            else: 
-                df = curve['df']
-                df['residuals'] = df.richards - df.casos_cum
+            else:
+                df = curve["df"]
+                df["residuals"] = df.richards - df.casos_cum
                 return df
-            
 
     def plot_fit(self, geocode, year=0):
         if year == 0:
@@ -224,10 +230,10 @@ class EpiScanner:
             y = curve["year"]
             df = curve["df"]
             df.set_index("data_iniSE", inplace=True)
-            df['residuals'] = curve['residuals']
+            df["residuals"] = curve["residuals"]
 
-            if not df.index.name == 'data_iniSE':
-                df.set_index('data_iniSE', inplace=True)
+            if not df.index.name == "data_iniSE":
+                df.set_index("data_iniSE", inplace=True)
             if year != 0 and y != year:
                 continue
 
@@ -235,26 +241,44 @@ class EpiScanner:
                 ax=axes[i], alpha=0.3, color="r", label=f"data_{y}", rot=45
             )
             df.richards.plot(ax=axes[i], label="model", use_index=True)
-            
-            df.residuals.plot(ax=axes[i],marker='+', label='Abs. residuals')
-            
-            ep_duration = curve['ep_time']
 
-            axes[i].axvline(df.loc[df.index == pd.to_datetime(Week.fromstring(ep_duration['ini']).startdate()) ].index.values[0], color = 'black', ls = '--')
-    
-            axes[i].axvline(df.loc[df.index == pd.to_datetime(Week.fromstring(ep_duration['end']).startdate()) ].index.values[0], color = 'black', ls = '--', label='Epidemic \n period')
-    
+            df.residuals.plot(ax=axes[i], marker="+", label="Abs. residuals")
+
+            ep_duration = curve["ep_time"]
+
+            axes[i].axvline(
+                df.loc[
+                    df.index
+                    == pd.to_datetime(
+                        Week.fromstring(ep_duration["ini"]).startdate()
+                    )
+                ].index.values[0],
+                color="black",
+                ls="--",
+            )
+
+            axes[i].axvline(
+                df.loc[
+                    df.index
+                    == pd.to_datetime(
+                        Week.fromstring(ep_duration["end"]).startdate()
+                    )
+                ].index.values[0],
+                color="black",
+                ls="--",
+                label="Epidemic \n period",
+            )
+
             axes[i].set_title(geocode)
 
             axes[i].legend()
             i += 1
         plt.tight_layout()
 
-
     def to_csv(self, fname_path):
         data = {
             "geocode": [],
-            'muni_name':[],
+            "muni_name": [],
             "year": [],
             "peak_week": [],
             "beta": [],
@@ -262,15 +286,15 @@ class EpiScanner:
             "R0": [],
             "total_cases": [],
             "alpha": [],
-            'sum_res':[],
-            'ep_ini':[],
-            'ep_end':[],
-            'ep_dur': []
+            "sum_res": [],
+            "ep_ini": [],
+            "ep_end": [],
+            "ep_dur": [],
         }
         for gc, curve in self.curves.items():
             for c in curve:
                 data["geocode"].append(gc)
-                data['muni_name'].append(self.muninames[gc]) 
+                data["muni_name"].append(self.muninames[gc])
                 data["year"].append(c["year"])
                 params = [
                     p["params"]
@@ -289,12 +313,12 @@ class EpiScanner:
                 data["gamma"].append(sir_params["gamma"])
                 data["R0"].append(sir_params["R0"])
 
-                # new columns 
-                data['sum_res'].append(c['sum_res'])
-                ep_duration = c['ep_time']
-                data['ep_ini'].append(ep_duration['ini'])
-                data['ep_end'].append(ep_duration['end'])
-                data['ep_dur'].append(ep_duration['dur'])
+                # new columns
+                data["sum_res"].append(c["sum_res"])
+                ep_duration = c["ep_time"]
+                data["ep_ini"].append(ep_duration["ini"])
+                data["ep_end"].append(ep_duration["end"])
+                data["ep_dur"].append(ep_duration["dur"])
 
         dfpars = pd.DataFrame(data)
         # Create a Path object for the file path
